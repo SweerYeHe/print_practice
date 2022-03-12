f744a9792f437007a0f2b6bde8ed48eb865e00b1
SweerYeHe <100355122+SweerYeHe@users.noreply.github.com>
2022-03-12 09:19:06 +0800

    把打印写成函数，新增同时打印姓名和地址函数print_name_adress(void)

--- print_practice/main.c
+++ print_practice/main.c
@@ -1,10 +1,29 @@
 # include <stdio.h>
 
+
+//函数原型   声明
+void print_name_adress(void);
+void print_name_adress(void);
+
 void main(void)
+{
+	print_name();//调用函数
+	print_name_adress();
+	return 0;
+}
+
+///打印姓名
+void print_name(void)
 {
 	printf("SweerYeHe\n");
 	printf("Sweer\n");
 	printf("YeHe\n");
 	printf("SweerYeHe\n");
-	return 0;
+}
+
+///打印姓名和地址
+void print_name_adress(void)//函数定义
+{
+	printf("Name:SweerYeHe\n");
+	printf("Adress:ShenYang\n");
 }
