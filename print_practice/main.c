48caf74ac7014066b0fbe1371f2c4e142b5a7456
SweerYeHe <100355122+SweerYeHe@users.noreply.github.com>
2022-03-12 10:15:07 +0800

    新增打印喜爱的话功能，新增函数void print_LoveSentence(void)

--- print_practice/main.c
+++ print_practice/main.c
@@ -3,11 +3,14 @@
 //函数原型   声明
 void print_name_adress(void);
 void print_name_adress(void);
+void print_LoveSentence(void);
+
 
 void main(void)
 {
 	print_name();//函数调用
 	print_name_adress();
+	print_LoveSentence();
 	return 0;
 }
 
@@ -26,4 +29,10 @@ void print_name_adress(void)
 {
 	printf("Name:SweerYeHe\n");
 	printf("Adress:ShenYang\n");
+}
+
+///打印一句话
+void print_LoveSentence(void)
+{
+	printf("“如果我们学习这么多知识，却不能用来帮助他人，那学来何用呢？”**\n");
 }
