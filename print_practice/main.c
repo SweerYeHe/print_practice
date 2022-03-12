4de2eacd575298ae933a50a0da0f6fa6f0375893
SweerYeHe <100355122+SweerYeHe@users.noreply.github.com>
2022-03-12 09:45:31 +0800

    没改

--- print_practice/main.c
+++ print_practice/main.c
@@ -1,17 +1,17 @@
 # include <stdio.h>
 
-
 //函数原型   声明
 void print_name_adress(void);
 void print_name_adress(void);
 
 void main(void)
 {
-	print_name();//调用函数
+	print_name();//函数调用
 	print_name_adress();
 	return 0;
 }
 
+//函数定义
 ///打印姓名
 void print_name(void)
 {
@@ -22,7 +22,7 @@ void print_name(void)
 }
 
 ///打印姓名和地址
-void print_name_adress(void)//函数定义
+void print_name_adress(void)
 {
 	printf("Name:SweerYeHe\n");
 	printf("Adress:ShenYang\n");
